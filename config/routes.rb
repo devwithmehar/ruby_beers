65509c5cfc93ab35e3ce31474b7eb7b416e14fb2
Sahil Mehar <meharsahil207@gmail.com>
2021-09-29 20:19:29 -0500

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get 'companies/index'
+  # get 'companies/show'
+  # DSL - Domain specific language
+  # making like an execute config file
+  get "/companies", to: "companies#index", as: "companies" # companies_path
+  get "/companies/:id", to: "companies#show", as: "company" # company_path
+  get "/yay", to: "companies#index", as: "yay"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
