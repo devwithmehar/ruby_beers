c1142e26d6ebd0ef1e5fe11ed9b4853023e4a922
Sahil Mehar <meharsahil207@gmail.com>
2021-10-08 18:10:42 -0500

    faker added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Name.unique.clear
+Faker::UniqueGenerator.clear
+
+Beer.delete_all
+Company.delete_all
+
+8.times do
+  company = Company.create(name: Faker::Company.unique.name)
+
+  number_of_beers_to_create = rand(4...8)
+
+  number_of_beers_to_create.times do
+  # Create beers with a particular company id
+    company.beers.create(
+      name: Faker::Beer.unique.name,
+      description: "This #{Faker::Beer.style} style of beer has #{Faker::Beer.alcohol}  alcohol ",
+      price: Faker::Commerce.price
+    )
+  end
+end
+
+puts "Created #{Company.count} Comanpies!"
+puts "Created #{Beer.count} Beers!"
